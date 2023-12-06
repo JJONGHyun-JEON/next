bf7f6394c0f9b2ce26a819812f887105ebdea47f
JJONGHyun-JEON <wjswhdgus153@gmail.com>
2023-12-06 19:36:45 +0900

    react->next시작

--- app/list/page.js
+++ app/list/page.js
@@ -8,7 +8,7 @@ export default function List() {
             상품.map((a, i)=>{
                 return (
                     <div className="food" key={i}>
-                        <img src='/food0'/>
+                        <img src='/food0'/> 
                     <h4>{상품[i]} $40</h4>
                     </div>
                 )
