1d507437107f8ccdceccf3cfa37ed0fb37413764
JJONGHyun-JEON <wjswhdgus153@gmail.com>
2023-12-06 19:36:20 +0900

    first commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  let name = 'JEON'
+  return (
+    <div>
+      <h4 className='title'>JJONG-FRESH</h4>
+      <p className='title-sub'> by dev {name}</p>
+    </div>
+  )
+}
